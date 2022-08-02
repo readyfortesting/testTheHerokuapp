3df6c628ffa5604d38cd6f39d00cafde5f957987
Tall Boy <tallboy6077@gmail.com>
2022-08-02 22:01:27 +0300

    removeEmptyUser

---
+++ testTheHerokuapp-main/src/test/java/test/removeEmptyUser.java
@@ -0,0 +1,32 @@
+package test;
+import base.BaseTest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import page.*;
+
+public class removeEmptyUser extends BaseTest {
+    LoginPage loginPage;
+    SecurePage securePage;
+    Utility utility;
+
+    @Before
+    public void setUpTest() {
+
+        loginPage = new LoginPage();
+        securePage = new SecurePage();
+        utility = new Utility();
+
+    }
+    @Test
+    public void removeEmptyUserPassMsg(){
+        loginPage.inputUsernameField("");
+        loginPage.inputPasswordField("");
+        loginPage.clickLoginButton();
+        Assert.assertTrue(loginPage.invalidUsernameTextIsDisplay());
+        Assert.assertEquals("Your username is invalid!\n" +
+                "×", loginPage.invalidUsernameTextIsGet());
+        securePage.clickCloseX();
+    }
+
+}
